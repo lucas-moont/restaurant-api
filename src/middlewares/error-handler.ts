3058a1814247cf0994ec6e40e360a567a1ce3d18
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-03-08 00:00:22 -0300

    --feat Installing zod and handling zod error.

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,10 +1,18 @@
 import { AppError } from "@/utils/AppError"
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 
 export function errorHandler(error: any, request: Request, response: Response, _: NextFunction) {
   if(error instanceof AppError){
     return response.status(error.statusCode).json({
       message: error.message
+    })
+  }
+
+  if(error instanceof ZodError){
+    return response.status(400).json({
+      message: 'validation error',
+      issues: error.format()
     })
   }
 
