11be78370b854a32be827104aef43078c2c1c9c9
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-03-09 01:03:34 -0300

    --feat Adding update controller and route for products.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -6,10 +6,13 @@ import z from "zod";
 class ProductController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
-      const { name } = req.query
+      const { name } = req.query;
 
-      const products = await knex<ProductRepository>("products").select("*").whereLike("name", `%${name ?? ''}%`).orderBy('name');
-      
+      const products = await knex<ProductRepository>("products")
+        .select("*")
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name");
+
       return res.json({ products });
     } catch (error) {
       next(error);
@@ -28,6 +31,49 @@ class ProductController {
       await knex<ProductRepository>("products").insert({ name, price });
 
       return res.status(201).json({ message: "Produto criado com sucesso" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(req.params.id);
+
+      const bodySchema = z.object({
+        name: z.string().optional(),
+        price: z.number().optional(),
+      });
+
+      const { name, price } = bodySchema.parse(req.body);
+
+      if(!name && !price) {
+        throw new AppError("You must provide at least one field to update", 400);
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .select("*")
+        .where({ id })
+        .first();
+
+      if (!product) {
+        throw new AppError("Product not found", 404);
+      }
+
+      await knex<ProductRepository>("products")
+        .update({
+          ...product,
+          name: name !== undefined ? name : product.name,
+          price: price !== undefined ? price : product.price,
+          updated_at: new Date(),
+        })
+        .where({ id });
+
+      res.json({ message: "Product updated successfully" });
     } catch (error) {
       next(error);
     }
