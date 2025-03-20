742575a3f23c48e09e5a911ea487cb9dfedec105
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-03-19 21:41:48 -0300

    --feat Feature that doesn't allow table to opened twice.

--- src/controllers/table-sessions-controller.ts
+++ src/controllers/table-sessions-controller.ts
@@ -1,33 +1,44 @@
 import { knex } from "@/database/knex";
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
-import z from 'zod'
+import z from "zod";
 
 class TableSessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
-    try{
+    try {
       const bodySchema = z.object({
         table_id: z.number(),
-      })
-      
-      const { table_id } = bodySchema.parse(req.body)
+      });
 
-      const table = await knex<TablesRepository>('tables').where('id', table_id).first()
+      const { table_id } = bodySchema.parse(req.body);
 
-      if(!table){
-        throw new AppError('Table not found', 404)
+      const table = await knex<TablesRepository>("tables")
+        .where("id", table_id)
+        .first();
+
+      if (!table) {
+        throw new AppError("Table not found", 404);
       }
 
-      await knex<TableSessionsRepository>('tables_sessions').insert({
-        table_id,
-        opened_at: knex.fn.now()
-      })  
+      const session = await knex<TableSessionsRepository>("tables_sessions")
+        .where({ table_id })
+        .orderBy("opened_at")
+        .first();
 
-      res.status(201).json()
-    }catch(error) {
-      next(error)
+        if(session && !session.closed_at) {
+          throw new AppError('This table is already opened', 400)
+        }
+
+      await knex<TableSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
+      res.status(201).json();
+    } catch (error) {
+      next(error);
     }
   }
 }
 
-export { TableSessionsController }
+export { TableSessionsController };
