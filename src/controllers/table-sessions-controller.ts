6bd0be35b4110b7b27a2d2b3d867628d1bfdb982
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-03-19 21:25:21 -0300

    --feat Adding feature for opening table.

--- src/controllers/table-sessions-controller.ts
+++ src/controllers/table-sessions-controller.ts
@@ -6,7 +6,24 @@ import z from 'zod'
 class TableSessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try{
-      return res.json({message: 'Table session created'})
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+      
+      const { table_id } = bodySchema.parse(req.body)
+
+      const table = await knex<TablesRepository>('tables').where('id', table_id).first()
+
+      if(!table){
+        throw new AppError('Table not found', 404)
+      }
+
+      await knex<TableSessionsRepository>('tables_sessions').insert({
+        table_id,
+        opened_at: knex.fn.now()
+      })  
+
+      res.status(201).json()
     }catch(error) {
       next(error)
     }
